3b6a8140ed32f0696a344fa50c970b87e8a18bcf
mansour9897 <mansour9897@gmail.com>
2023-04-14 16:04:38 +0330

    define ads8344 header file

--- ads8344.h
+++ ads8344.h
@@ -1,65 +1,24 @@
-// Include the pico libraries
+#ifndef __ads8344__
+#define __ads8344__
+
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
 
-// Define the SPI pins
-#define SPI_PORT spi0
-#define PIN_MISO 16
-#define PIN_CS   17
-#define PIN_SCK  18
-#define PIN_MOSI 19
-
+//Selects Internal Clock Mode
+//Single-Ended Channel Selection
 // Define the ADC channels
-#define CH0 0b10000000
-#define CH1 0b10010000
-#define CH2 0b10100000
-#define CH3 0b10110000
-#define CH4 0b11000000
-#define CH5 0b11010000
-#define CH6 0b11100000
-#define CH7 0b11110000
+#define CH0 0b10000110
+#define CH1 0b11000110
+#define CH2 0b10010110
+#define CH3 0b11010110
+#define CH4 0b10100110
+#define CH5 0b11100110
+#define CH6 0b10110110
+#define CH7 0b11110110
 
 // Define a function to read data from a channel
-uint16_t read_adc(uint8_t channel) {
-    // Select the channel by sending a command byte
-    spi_write_blocking(SPI_PORT, &channel, 1);
+uint16_t read_adc(spi_inst_t *spi,uint8_t channel);
 
-    // Wait for the conversion to finish (about 8 us)
-    sleep_us(8);
 
-    // Read two bytes of data from the ADC
-    uint8_t data[2];
-    spi_read_blocking(SPI_PORT, 0, data, 2);
-
-    // Combine the two bytes into a 12-bit value
-    uint16_t value = (data[0] << 8) | data[1];
-    value >>= 4;
-
-    // Return the value
-    return value;
-}
-
-int main() {
-    // Initialize the SPI module with a clock speed of 1 MHz
-    spi_init(SPI_PORT, 1000000);
-
-    // Set up the SPI pins
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-
-    // Set the chip select pin as output and high
-    gpio_init(PIN_CS);
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-
-    while (true) {
-        // Read data from channel 0 and print it to the serial monitor
-        uint16_t value = read_adc(CH0);
-        printf("Channel 0: %d\n", value);
-
-        // Wait for a second
-        sleep_ms(1000);
-    }
-}
+#endif
