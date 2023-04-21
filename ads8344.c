0e8e26a29e6a3106a4384be87aa1531a9ffd2fad
mansour9897 <mansour9897@gmail.com>
2023-04-21 17:09:55 +0330

    add read continus function

--- ads8344.c
+++ ads8344.c
@@ -7,7 +7,7 @@ uint16_t ads_read(spi_inst_t *spi, uint8_t channel)
     spi_write_blocking(spi, &channel, 1);
 
     // Wait for the conversion to finish (about 8 us)
-    sleep_us(8);
+    // sleep_us(8);
 
     // Read two bytes of data from the ADC
     uint8_t data[3];
@@ -18,4 +18,21 @@ uint16_t ads_read(spi_inst_t *spi, uint8_t channel)
 
     // Return the value
     return value;
+}
+
+void ads_read_data_continues(spi_inst_t *spi, uint8_t *channels, uint16_t *data, uint32_t len)
+{
+    uint32_t data_index = 0;
+    uint8_t ch_index = 0;
+    // uint8_t ch = channels[ch_index++];
+    spi_write_blocking(spi, &channels[ch_index++], 1);
+    while (len > data_index)
+    {
+        uint8_t d[3];
+        spi_write_read_blocking(spi, &channels[ch_index], d, 3);
+        data[data_index++] = ((d[0] << 16) | (d[1] << 8) | d[2]) >> 7;
+        ch_index += 3;
+        if (ch_index >= 24)
+            ch_index = 1;
+    }
 }
--- ads8344.h
+++ ads8344.h
@@ -5,19 +5,20 @@
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
 
-//Selects Internal Clock Mode
-//Single-Ended Channel Selection
-// Define the ADC channels
-#define CH0 0b10000110
-#define CH1 0b11000110
-#define CH2 0b10010110
-#define CH3 0b11010110
-#define CH4 0b10100110
-#define CH5 0b11100110
-#define CH6 0b10110110
-#define CH7 0b11110110
+// Selects Internal Clock Mode
+// Single-Ended Channel Selection
+//  Define the ADC channels
+#define CH0 0b10000111
+#define CH1 0b11000111
+#define CH2 0b10010111
+#define CH3 0b11010111
+#define CH4 0b10100111
+#define CH5 0b11100111
+#define CH6 0b10110111
+#define CH7 0b11110111
 
 // Define a function to read data from a channel
-uint16_t ads_read(spi_inst_t *spi,uint8_t channel);
+uint16_t ads_read(spi_inst_t *spi, uint8_t channel);
+void ads_read_data_continues(spi_inst_t *spi, uint8_t *channels, uint16_t *data, uint32_t len);
 
 #endif
